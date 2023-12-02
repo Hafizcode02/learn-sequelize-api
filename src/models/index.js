e5f1388b15e608a4fb81b0a7b480c05803cc194a
Hafiz Caniago <hafizcode02@gmail.com>
2023-12-02 13:28:21 +0700

    configuration relation and add function initdb

--- src/models/index.js
+++ src/models/index.js
@@ -1,10 +1,10 @@
 const dbConfig = require('../config/db.config');
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-    dbConfig.DB,
-    dbConfig.USER,
-    dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
+    dbConfig.database,
+    dbConfig.username,
+    dbConfig.password, {
+    host: dbConfig.host,
     dialect: dbConfig.dialect,
     operatorAlias: false,
     pool: {
@@ -23,4 +23,33 @@ db.books = require('./book.model')(sequelize, Sequelize);
 db.user = require('./user.model')(sequelize, Sequelize);
 db.role = require('./role.model')(sequelize, Sequelize);
 db.refreshToken = require('./refreshToken.model')(sequelize, Sequelize);
-module.exports = db;
+
+// assign relation database
+db.role.hasMany(db.user, {
+    foreignKey: "role",
+});
+db.refreshToken.belongsTo(db.user, {
+    foreignKey: "userId",
+    targetKey: "id",
+});
+db.user.hasOne(db.refreshToken, {
+    foreignKey: "userId",
+    targetKey: "id",
+});
+
+db.ROLES = ["user", "admin", "superadmin"];
+
+const initializeDatabase = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully.");
+
+        await sequelize.sync(); // Sync models with the database
+        console.log("Database synchronized.");
+    } catch (error) {
+        console.error("Unable to connect to the database:", error.message);
+        throw error; // rethrow the error to be caught in server.js
+    }
+};
+
+module.exports = { db, initializeDatabase };
