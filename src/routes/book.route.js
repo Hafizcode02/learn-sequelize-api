d1d851634f82cbd053f6db40d395bb988d1ca739
Hafiz Caniago <hafizcode02@gmail.com>
2023-12-02 13:29:01 +0700

    test implement jwt auth in another endpoint

--- src/routes/book.route.js
+++ src/routes/book.route.js
@@ -1,11 +1,12 @@
 const { celebrate, Segments } = require('celebrate');
 const bookController = require('../controllers/book.controller');
 const schemas = require('../utils/book.schema');
+const { authJwt } = require('../middleware');
 const router = require('express').Router();
 
 // Celebrate used as middleware
 router.post('/', celebrate({ [Segments.BODY]: schemas.bookPost }), bookController.create);
-router.get('/', bookController.findAll);
+router.get('/', [authJwt.verifyToken, authJwt.isAdmin],bookController.findAll);
 router.put('/:id', celebrate({ [Segments.BODY]: schemas.bookUpdate }), bookController.update);
 router.put('/:id', bookController.update);
 router.delete('/:id', bookController.delete);
