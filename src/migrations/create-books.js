77170a7f1e94e94513052adf19218ab9c0a5840a
Hafiz Caniago <hafizcode02@gmail.com>
2023-12-02 21:10:44 +0700

    create migration for book table

---
+++ src/migrations/create-books.js
@@ -0,0 +1,40 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        /**
+         * Add altering commands here.
+         *
+         * Example:
+         * await queryInterface.createTable('books', { id: Sequelize.INTEGER });
+         */
+        await queryInterface.createTable("books", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            title: {
+                type: Sequelize.STRING,
+            },
+            description: {
+                type: Sequelize.TEXT,
+            },
+            published: {
+                type: Sequelize.BOOLEAN,
+            },
+        });
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add reverting commands here.
+         *
+         * Example:
+         * await queryInterface.dropTable('users');
+         */
+        await queryInterface.dropTable("books");
+    },
+};
