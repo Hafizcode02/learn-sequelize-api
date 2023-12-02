c007c27656e422ac33a5ff2175958f2d212eada7
Hafiz Caniago <hafizcode02@gmail.com>
2023-12-02 13:27:05 +0700

    change format like db config in sequelize docs

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 // Configuration DB
 module.exports = {
-    HOST: process.env.DB_HOST,
-    USER: process.env.DB_USER,
-    PASSWORD: process.env.DB_PASSWORD,
-    DB: process.env.DB_DATABASE,
+    // HOST: process.env.DB_HOST,
+    // USER: process.env.DB_USER,
+    // PASSWORD: process.env.DB_PASSWORD,
+    // DB: process.env.DB_DATABASE,
+    // dialect: 'mysql',
+    username: process.env.DB_USER, // harus pake yang kecil gini kaya di dokumentasi
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     pool: {
         max: 5,
